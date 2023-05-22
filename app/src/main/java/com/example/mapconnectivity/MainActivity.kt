7971857b4c86a676da13681db23a3ed3a09d2d13
Zaid1710 <zaid1234@hotmail.it>
2023-05-22 22:08:43 +0200

    Add Wifi and "LTE" strength

--- app/src/main/java/com/example/mapconnectivity/MainActivity.kt
+++ app/src/main/java/com/example/mapconnectivity/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.mapconnectivity
 
+import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
 import android.media.MediaRecorder
+import android.net.wifi.WifiManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.telephony.CellInfoLte
+import android.telephony.TelephonyManager
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
@@ -15,21 +20,80 @@ import kotlin.math.log10
 
 class MainActivity : AppCompatActivity() {
     private lateinit var microphoneText: TextView
+    private lateinit var wifiText: TextView
 
     @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val fetchBtn = findViewById<Button>(R.id.fetchBtn)
+        val microphoneBtn = findViewById<Button>(R.id.microphoneBtn)
+        val lteBtn = findViewById<Button>(R.id.lteBtn)
+        val wifiBtn = findViewById<Button>(R.id.wifiBtn)
         microphoneText = findViewById(R.id.microphoneText)
+        wifiText = findViewById(R.id.wifiText)
 
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.RECORD_AUDIO), 0)
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECORD_AUDIO), 0)
         }
 
-        fetchBtn.setOnClickListener { fetchMicrophone() }
+        microphoneBtn.setOnClickListener { fetchMicrophone() }
+        lteBtn.setOnClickListener { fetchLTE() }
+        wifiBtn.setOnClickListener { fetchWifi() }
     }
 
+    @RequiresApi(Build.VERSION_CODES.R)
+
+    private fun fetchWifi() {
+        Thread {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_WIFI_STATE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_WIFI_STATE), 7)
+
+            }
+            val wfm2: WifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+            var maxLevel = -200
+            for(scanResult in wfm2.scanResults) {
+                if (scanResult.level > maxLevel) {
+                    maxLevel = scanResult.level
+                }
+            }
+            runOnUiThread {
+                Log.d("WIFI", maxLevel.toString())
+                wifiText.text = maxLevel.toString()
+            }
+        }.start()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    private fun fetchLTE() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), 1)
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 2)
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_BACKGROUND_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION), 3)
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_NUMBERS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_PHONE_NUMBERS), 4)
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_PHONE_STATE), 5)
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_SMS), 6)
+        }
+
+        try {
+
+            val tm: TelephonyManager = this.getSystemService(TELEPHONY_SERVICE) as TelephonyManager
+            val infoLte = tm.allCellInfo[2] as CellInfoLte
+            val lte = infoLte.cellSignalStrength.dbm
+            Log.d("LTE", "LTE: $lte")
+        } catch (e: Throwable) {
+            Log.wtf("LTE", e)
+        }
+    }
 
     /* Registra 5 secondi l'audio ambientale dal microfono e calcola la media dei dB recepiti */
     @RequiresApi(Build.VERSION_CODES.S)
