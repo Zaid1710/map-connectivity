80e2c98a31acdb6cd81c2c273a3c7de3a389ea31
Zaid1710 <zaid1234@hotmail.it>
2023-07-08 22:25:56 +0200

    Changed export file name

--- app/src/main/java/com/example/mapconnectivity/SwapActivity.kt
+++ app/src/main/java/com/example/mapconnectivity/SwapActivity.kt
@@ -1,7 +1,7 @@
 package com.example.mapconnectivity
 
 import android.content.Intent
-import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -9,6 +9,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.ProgressBar
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.core.content.FileProvider
 import androidx.room.Room
 import com.fasterxml.jackson.databind.ObjectMapper
@@ -17,9 +18,10 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import org.json.JSONObject
 import java.io.File
 import java.io.IOException
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 class SwapActivity : AppCompatActivity() {
     private lateinit var importBtn: Button
@@ -29,6 +31,7 @@ class SwapActivity : AppCompatActivity() {
     private lateinit var mapper: ObjectMapper
     private lateinit var exportProgressBar: ProgressBar
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_swap)
@@ -49,6 +52,7 @@ class SwapActivity : AppCompatActivity() {
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     private fun exportData() {
         CoroutineScope(Dispatchers.IO).launch {
             withContext(Dispatchers.Main) {
@@ -57,8 +61,12 @@ class SwapActivity : AppCompatActivity() {
             }
             mapper = jacksonObjectMapper()
             val measures = measureDao.getAllMeasures()
+
+            val now = ZonedDateTime.now()
+            val formatter = DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss")
+            val currTime = now.format(formatter)
             try {
-                val file = File("${applicationContext.filesDir}/export.json")
+                val file = File("${applicationContext.filesDir}/export_${currTime}.mapc")
                 Log.d("FILE", file.toString())
                 mapper.writeValue(file, measures)
                 withContext(Dispatchers.Main) {
