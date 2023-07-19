c2219419856b9646791a360b8e754034731c2ea8
Zaid1710 <zaid1234@hotmail.it>
2023-07-19 16:33:01 +0200

    Init map grid implementation

--- app/src/main/java/com/example/mapconnectivity/Map.kt
+++ app/src/main/java/com/example/mapconnectivity/Map.kt
@@ -20,6 +20,8 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import kotlin.math.ceil
+import kotlin.math.floor
 
 
 class Map(mapView: SupportMapFragment?, activity: MainActivity) {
@@ -87,15 +89,15 @@ class Map(mapView: SupportMapFragment?, activity: MainActivity) {
 
                         googleMap.setOnCameraIdleListener {
                             drawGridOnMap(googleMap, mode)
-                            Log.d("POLIGONOATTUALE", getCurrentPolygon(getPosition()).toString())
-                            val currpoly = getCurrentPolygon(getPosition())
-                            if (currpoly != null) {
-                                currpoly.fillColor = Color.RED
-                            }
+//                            Log.d("POLIGONOATTUALE", getCurrentPolygon(getPosition()).toString())
+//                            val currpoly = getCurrentPolygon(getPosition())
+//                            if (currpoly != null) {
+//                                currpoly.fillColor = Color.RED
+//                            }
                         }
 
                         googleMap.setOnCameraMoveListener {
-                            deleteGrid()
+//                            deleteGrid()
                         }
                     }
                 }
@@ -104,83 +106,119 @@ class Map(mapView: SupportMapFragment?, activity: MainActivity) {
 
     fun drawGridOnMap(googleMap: GoogleMap, mode: Int) {
         CoroutineScope(Dispatchers.IO).launch {
-            withContext(Dispatchers.Main) { deleteGrid() }
-            var bounds = withContext(Dispatchers.Main) { googleMap.projection.visibleRegion.latLngBounds }
+            val bounds = withContext(Dispatchers.Main) { googleMap.projection.visibleRegion.latLngBounds }
+            val meters = 100.0
+            val tlPoint = calculateGridSize(meters, bounds.northeast.latitude, bounds.southwest.longitude) // tl significa Top Left
+            var trPoint = LatLng(tlPoint.latitude + metersToOffset(meters), tlPoint.longitude)
+            var blPoint = LatLng(tlPoint.latitude, tlPoint.longitude + metersToOffset(meters))
+            var brPoint = LatLng(tlPoint.latitude + metersToOffset(meters), tlPoint.longitude + metersToOffset(meters))
 
-            val topLeft = bounds.northeast
-            val bottomRight = bounds.southwest
-            val gridSize = 10
+            val polygonOptions = PolygonOptions()
+                .add(tlPoint, trPoint, brPoint, blPoint)
+//                .strokeWidth(2f)
+                .strokeWidth(5f)
+                .strokeColor(Color.BLACK) // Colore del bordo (nero)
+                .fillColor(Color.argb(128, 255, 0, 0)) // Colore di riempimento
 
-            database = Room.databaseBuilder(activity, MeasureDB::class.java, "measuredb")
-                .fallbackToDestructiveMigration().build()
-            var measureDao = database.measureDao()
+            withContext(Dispatchers.Main) {
+                val polygon = googleMap.addPolygon(polygonOptions)
 
-            val cellWidth = (bottomRight.longitude - topLeft.longitude) / gridSize
-            val cellHeight = (topLeft.latitude - bottomRight.latitude) / gridSize
+//                Log.d("QUADRATO", polygon.points.toString())
 
-            for (i in 0 until gridSize) {
-                for (j in 0 until gridSize) {
-                    val latLng1 =
-                        LatLng(topLeft.latitude - i * cellHeight, topLeft.longitude + j * cellWidth)
-                    val latLng2 = LatLng(
-                        topLeft.latitude - (i + 1) * cellHeight,
-                        topLeft.longitude + j * cellWidth
-                    )
-                    val latLng3 = LatLng(
-                        topLeft.latitude - (i + 1) * cellHeight,
-                        topLeft.longitude + (j + 1) * cellWidth
-                    )
-                    val latLng4 = LatLng(
-                        topLeft.latitude - i * cellHeight,
-                        topLeft.longitude + (j + 1) * cellWidth
-                    )
+//                polygon.tag = "Polygon($i,$j)"
 
-                    var color = Color.TRANSPARENT
-
-                    val prefs = PreferenceManager.getDefaultSharedPreferences(activity)
-                    val imported = prefs.getBoolean("view_imported", true)
-
-                    val measurements = measureDao.getAvgMeasuresInPolygon(
-                        latLng1.latitude,
-                        latLng1.longitude,
-                        latLng3.latitude,
-                        latLng3.longitude,
-                        imported
-                    )
-                    Log.d("BABA", measurements.toString())
-                    var avgModeMeasure: Double? = 0.0
-                    var lowerBound = 0.0
-                    var upperBound = 0.0
-                    when (mode) {
-                        LTE -> { avgModeMeasure = measurements.avgLte; lowerBound = LTE_BAD; upperBound = LTE_OPT }
-                        WIFI -> { avgModeMeasure = measurements.avgWifi; lowerBound = WIFI_BAD; upperBound = WIFI_OPT }
-                        DB -> { avgModeMeasure = measurements.avgDb?.times(-1); lowerBound = DB_BAD; upperBound = DB_OPT }
-                    }
-                    if (avgModeMeasure != null) {
-                        color = getQuality(avgModeMeasure, lowerBound, upperBound)
-                    }
-
-                    val polygonOptions = PolygonOptions()
-                        .add(latLng1, latLng2, latLng3, latLng4)
-                        .strokeWidth(2f)
-                        .strokeColor(Color.BLACK) // Colore del bordo (nero)
-                        .fillColor(color) // Colore di riempimento
-
-                    withContext(Dispatchers.Main) {
-                        val polygon = googleMap.addPolygon(polygonOptions)
-
-                        polygon.tag = "Polygon($i,$j)"
-
-                        Log.d("Punticini", polygon.points[0].latitude.toString())
-                        gridPolygons.add(polygon)
-                    }
-                }
+//                Log.d("Punticini", polygon.points[0].latitude.toString())
+                gridPolygons.add(polygon)
             }
         }
     }
 
-    private fun calculateGridSize(zoom: Float): Int {
-        return (zoom).toInt()
+//    fun drawGridOnMap(googleMap: GoogleMap, mode: Int) {
+//        CoroutineScope(Dispatchers.IO).launch {
+//            withContext(Dispatchers.Main) { deleteGrid() }
+//            var bounds = withContext(Dispatchers.Main) { googleMap.projection.visibleRegion.latLngBounds }
+//
+//            val topLeft = bounds.northeast
+//            val bottomRight = bounds.southwest
+//            val gridSize = 10
+//
+//            database = Room.databaseBuilder(activity, MeasureDB::class.java, "measuredb")
+//                .fallbackToDestructiveMigration().build()
+//            var measureDao = database.measureDao()
+//
+//            val cellWidth = (bottomRight.longitude - topLeft.longitude) / gridSize
+//            val cellHeight = (topLeft.latitude - bottomRight.latitude) / gridSize
+//
+//            for (i in 0 until gridSize) {
+//                for (j in 0 until gridSize) {
+//                    val latLng1 =
+//                        LatLng(topLeft.latitude - i * cellHeight, topLeft.longitude + j * cellWidth)
+//                    val latLng2 = LatLng(
+//                        topLeft.latitude - (i + 1) * cellHeight,
+//                        topLeft.longitude + j * cellWidth
+//                    )
+//                    val latLng3 = LatLng(
+//                        topLeft.latitude - (i + 1) * cellHeight,
+//                        topLeft.longitude + (j + 1) * cellWidth
+//                    )
+//                    val latLng4 = LatLng(
+//                        topLeft.latitude - i * cellHeight,
+//                        topLeft.longitude + (j + 1) * cellWidth
+//                    )
+//
+//                    var color = Color.TRANSPARENT
+//
+//                    val prefs = PreferenceManager.getDefaultSharedPreferences(activity)
+//                    val imported = prefs.getBoolean("view_imported", true)
+//
+//                    val measurements = measureDao.getAvgMeasuresInPolygon(
+//                        latLng1.latitude,
+//                        latLng1.longitude,
+//                        latLng3.latitude,
+//                        latLng3.longitude,
+//                        imported
+//                    )
+//                    Log.d("BABA", measurements.toString())
+//                    var avgModeMeasure: Double? = 0.0
+//                    var lowerBound = 0.0
+//                    var upperBound = 0.0
+//                    when (mode) {
+//                        LTE -> { avgModeMeasure = measurements.avgLte; lowerBound = LTE_BAD; upperBound = LTE_OPT }
+//                        WIFI -> { avgModeMeasure = measurements.avgWifi; lowerBound = WIFI_BAD; upperBound = WIFI_OPT }
+//                        DB -> { avgModeMeasure = measurements.avgDb?.times(-1); lowerBound = DB_BAD; upperBound = DB_OPT }
+//                    }
+//                    if (avgModeMeasure != null) {
+//                        color = getQuality(avgModeMeasure, lowerBound, upperBound)
+//                    }
+//
+//                    val polygonOptions = PolygonOptions()
+//                        .add(latLng1, latLng2, latLng3, latLng4)
+//                        .strokeWidth(2f)
+//                        .strokeColor(Color.BLACK) // Colore del bordo (nero)
+//                        .fillColor(color) // Colore di riempimento
+//
+//                    withContext(Dispatchers.Main) {
+//                        val polygon = googleMap.addPolygon(polygonOptions)
+//
+//                        polygon.tag = "Polygon($i,$j)"
+//
+//                        Log.d("Punticini", polygon.points[0].latitude.toString())
+//                        gridPolygons.add(polygon)
+//                    }
+//                }
+//            }
+//        }
+//    }
+
+
+    private fun calculateGridSize(meters: Double, lat: Double, lon: Double) : LatLng {
+        val offset = metersToOffset(meters)
+
+        return LatLng(ceil(lat / offset) * offset, floor(lon / offset) * offset)
+    }
+
+    private fun metersToOffset(meters: Double) : Double {
+        return meters / 111111.0
     }
 
     // Rimuove i poligoni della griglia precedente dalla mappa, se presenti
